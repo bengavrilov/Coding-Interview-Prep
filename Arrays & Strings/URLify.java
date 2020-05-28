492292394d5c15e8b2900c9e60661d61a4e8e829
Ben Gavrilov <bengavrilov@gmail.com>
2020-05-28 18:29:21 -0400

    added exercise 1.3

---
+++ Arrays & Strings/URLify.java
@@ -0,0 +1,39 @@
+
+// Solution
+
+public class URLify {
+    void replaceSpaces(char [] str, int trueLength) {
+        int spaceCount = 0, index, i = 0;
+        for (i = 0; i <trueLength; i++) {
+            if (str[i] == ' ') {
+                spaceCount++;
+            }
+        }
+
+        index = trueLength + spaceCount * 2;
+
+        if (trueLength < str.length) {
+            str[trueLength] = '\0';
+        }
+
+        for (i = trueLength - 1; i >= 0; i--) {
+            if (str[i] == ' ') {
+                str[index - 1] = '0';
+                str[index - 2] = '2';
+                str[index - 3] = '%';
+                index = index - 3;
+            }
+            else {
+                str[index - 1] = str[i];
+                index--;
+            }
+        }
+    }
+}
+
+/*
+ * Summary
+ * Two scans:
+ * First scan: count number of spaces
+ * Second scan: iterate from last index and replace spaces
+*/
